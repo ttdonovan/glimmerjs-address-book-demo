0afedde54a4b25ec388c1587d513d0ad1ff9cba1
Tanner Donovan <ttdonovan@gmail.com>
2017-04-19 18:02:14 -0700

    display a list of generated contacts

--- src/ui/components/address-book/component.ts
+++ src/ui/components/address-book/component.ts
@@ -1,5 +1,13 @@
-import Component from "@glimmer/component";
+import Component, { tracked } from "@glimmer/component";
+import Contact from '../../../utils/contact';
 
 export default class AddressBook extends Component {
+  @tracked contacts = generateContacts()
+}
 
+function generateContacts() {
+  return [
+    new Contact("John", "Smith"),
+    new Contact("Jane", "Doe")
+  ];
 }
---
+++ src/utils/contact.ts
@@ -0,0 +1,16 @@
+import { tracked } from "@glimmer/component";
+
+export default class Contact {
+  @tracked firstName: string;
+  @tracked lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  @tracked('firstName', 'lastName')
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`
+  }
+}
