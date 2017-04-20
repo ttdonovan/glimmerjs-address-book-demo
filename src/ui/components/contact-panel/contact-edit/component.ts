cf0ef1c9a4566b14127f471cf5cf613ef3821fef
Tanner Donovan <ttdonovan@gmail.com>
2017-04-19 19:46:00 -0700

    add handleEditKeyUp event to update contact

--- src/ui/components/contact-panel/contact-edit/component.ts
+++ src/ui/components/contact-panel/contact-edit/component.ts
@@ -1,6 +1,15 @@
-import Component, { tracked } from '@glimmer/component';
+import Component from '@glimmer/component';
 import Contact from '../../../../utils/contact';
 
 export default class ContactEdit extends Component {
   args: { contact: Contact };
+
+  handleEditKeyUp(event) {
+    const contact = this.args.contact;
+
+    const name = event.target.name;
+    const value = event.target.value.trim();
+
+    contact[name] = value;
+  }
 }
